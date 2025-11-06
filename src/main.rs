33ddf8a0cd8ec9fd97266802b41505a532d55fbe
Darren Weng <wjhdarren@gmail.com>
2025-11-06 22:58:10 +0800

    Add basic interpreter framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use anyhow::{Context, Result};
+use std::io::{self, Write};
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("Error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => {
+            eprintln!("Usage: rlox [script]");
+            process::exit(64); // EX_USAGE from sysexits.h
+        }
+    }
+}
+
+fn run_file(path: &str) -> Result<()> {
+    let content = fs::read_to_string(path)?;
+    run_source(content)
+}
+
+fn run_prompt() -> Result<()> {
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+        let mut line = String::new();
+        let bytes_read = io::stdin().read_line(&mut line)?;
+        if bytes_read == 0 {
+            break;
+        }
+        if line.trim().is_empty() {
+            continue;
+        }
+        if let Err(e) = run_source(line) {
+            eprintln!("Error: {e}");
+        }
+    }
+    Ok(())
+}
+
+fn run_source(source: String) -> Result<()> {
+    println!("{source}");
+    Ok(())
 }
